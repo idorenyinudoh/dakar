0199586731e3715a851069127becd444d589817e
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2025-08-19 01:40:03 +0100

    feat: install Lenis for smooth scrolling and configure plugin

---
+++ plugins/lenis.client.ts
@@ -0,0 +1,25 @@
+import Lenis from 'lenis'
+
+export default defineNuxtPlugin(() => {
+  if (process.client) {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+    })
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    return {
+      provide: {
+        lenis
+      }
+    }
+  }
+})
