6adfe42b4b40cd484d48feb0a25cd97fbdfebe29
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2025-08-19 15:42:37 +0100

    refactor: replace gsap imports in hero component with useGSAP composable and move video poster image from assets to public

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger)
+const { gsap } = useGSAP()
 
 const videoEl = useTemplateRef('video')
 
@@ -28,7 +25,7 @@ onMounted(() => {
   <header class="relative h-[150vh]">
     <h1 class="sticky top-3/4 md:top-5/6 z-20 py-[18px] px-4 text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-medium text-center -tracking-[3px] lg:-tracking-[7px] xl:-tracking-[8px] text-white opacity-0">The Dakar Renaissance</h1>
     <div class="parallax-vid absolute inset-0 overflow-hidden">
-      <video ref="video" src="https://res.cloudinary.com/idorenyinudoh/video/upload/v1755562025/dakar/dakar_crjvjy.mp4" class="object-cover w-full h-[110%]" autoplay muted loop playsinline preload="auto" poster="~/assets/images/dakar-poster.png" />
+      <video ref="video" src="https://res.cloudinary.com/idorenyinudoh/video/upload/v1755562025/dakar/dakar_crjvjy.mp4" class="object-cover w-full h-[110%]" autoplay muted loop playsinline preload="auto" poster="/dakar-poster.png" />
     </div>
   </header>
 </template>
