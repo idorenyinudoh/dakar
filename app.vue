4c6bfd5d1ea0f7252c1d905c360ff545e4392644
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2025-08-19 01:35:00 +0100

    feat: implement preloader animation and create hero component with video background

--- app.vue
+++ app.vue
@@ -1,5 +1,89 @@
+<script setup lang="ts">
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
+
+gsap.registerPlugin(SplitText)
+
+const preloaderAnimation = () => {
+  window.scrollTo(0, 0)
+  document.body.style.position = 'fixed'
+  document.body.style.top = '0'
+  document.body.style.left = '0'
+  document.body.style.width = '100%'
+
+  SplitText.create('.preloader-text', {
+    type: 'words',
+    mask: 'words',
+    wordsClass: 'word++',
+    onSplit: (self) => {
+      gsap.from(self.words, {
+        y: 100,
+        autoAlpha: 0,
+        duration: 3,
+        ease: 'expo.inOut',
+        stagger: 0.1,
+        onComplete: () => {
+          gsap.to(self.masks[5], {
+            opacity: 0,
+            duration: 1,
+            width: '+=10px',
+            ease: 'power4.out',
+            onComplete: () => {
+              self.masks[5].textContent = '2025'
+              gsap.to(self.masks[5], {
+                opacity: 1,
+                duration: 1.5,
+                ease: 'power4.out',
+                delay: 0.2,
+                onComplete: () => {
+                  const tl = gsap.timeline({
+                    onComplete: () => {
+                      document.body.style.position = 'static'
+                      document.body.style.top = ''
+                      document.body.style.left = ''
+                      document.body.style.width = ''
+                      gsap.set('.preloader', { display: 'none' })
+                    }
+                  })
+
+                  tl.to(self.masks, {
+                    opacity: 0,
+                    duration: 2,
+                    stagger: 0.1,
+                    ease: 'power4.out'
+                  })
+                  .to('.preloader', {
+                    y: '-100%',
+                    duration: 1.5,
+                    ease: 'power3.inOut'
+                  }, '-=1.5')
+                  .to('.body', {
+                    marginTop: 0,
+                    opacity: 1,
+                    duration: 1.5,
+                    ease: 'power3.inOut'
+                  }, '-=1.5')
+                }
+              })
+            }
+          })
+        }
+      })
+    }
+  })
+}
+
+onMounted(() => {
+  preloaderAnimation()
+})
+</script>
+
 <template>
   <div>
-    <p class="text-6xl">dakar</p>
+    <ThePreloader />
+    <div class="body mt-[100vh] opacity-0">
+      <TheHero />
+      <div class="flex justify-center items-center h-[100svh]">dakar</div>
+    </div>
   </div>
 </template>
